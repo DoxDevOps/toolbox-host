abf0031a3890a99181165f00e004e1485b78d4fb
lwara <lwaratiwo94@gmail.com>
2021-06-06 18:59:32 +0200

    fixed Emastercard Function

--- app.py
+++ app.py
@@ -1,111 +1,114 @@
+import socket
+from sys import stdout
+
 import flask
-import psutil as psutil
-import qrcode
+import os
 import psutil
+import qrcode
+import subprocess
 from psutil._common import bytes2human
 import psutil._common
 from flask import Flask, render_template
 
 #from fabric import *
 from os.path import exists
-
 from invoke import run
 
 app = Flask(__name__, static_folder="templates/static")
+
+#app = Flask(__name__)
 
 _emc_dir = "/var/www/emastercard-upgrade-automation"
 _poc_api_dir = "/var/www/BHT-EMR-API"
 _poc_core_dir = "/var/www/BHT-Core"
 _poc_core_art_dir = "/var/www/BHT-Core/apps/ART"
+_con = socket.gethostbyname(socket.gethostname())
+#_con = flask.request.remote_addr
+# **************sample data will be taken from the following functions*********************
 
 
-#**************sample data will be taken from the following functions*********************
+# Create QR Code Instance. It determines the size of the QR code
+qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
 
 
-#Create QR Code Instance. It determines the size of the QR code
-qr = qrcode.QRCode(version=1, box_size=10, border=5)
+# Gets all information about Random Access Memory (RAM)
 
-
-#Gets all information about Random Access Memory (RAM)
 def get_ram_details(ram):
     qr.add_data("MEMORY\n")
     for name in ram._fields:
         value = getattr(ram, name)
         if name != 'percent':
             value = bytes2human(value)
-        add_qr_data('%-10s : %7s' % (name.capitalize(), value)+'\n') #add to QR image
+        add_qr_data('%-10s : %7s' % (name.capitalize(), value) + '\n')  # add to QR image
 
-#Gets all information about Hard Disk Drive storage
+
+# Gets all information about Hard Disk Drive storage
 def get_disk_usage(disk_space):
     qr.add_data("DISK SPACE\n")
     for name in disk_space._fields:
         value = getattr(disk_space, name)
         if name != 'percent':
             value = bytes2human(value)
-        add_qr_data('%-10s : %7s' % (name.capitalize(), value)+'\n') #add to QR image
+        add_qr_data('%-10s : %7s' % (name.capitalize(), value) + '\n')  # add to QR image
 
-#Gets POC information
-def get_poc_versions(core, art, api):
+
+# Gets POC information
+def get_poc_versions():
     qr.add_data("\n POC VERSION \n")
-    api_result = run('cd ', _poc_api_dir | 'git describe --tags')
-    core_result = run('cd ', _poc_core_dir | 'git describe --taps')
+    api_result = subprocess.run('cd ', _poc_api_dir | 'git describe --tags')
+    core_result = subprocess.run('cd ', _poc_core_dir | 'git describe --taps')
     art_result = run('cd', _poc_core_art_dir | 'git describe --tags')
 
-    add_qr_data('%-10s : %7s' % ("API ", api_result)+'\n')
-    add_qr_data('%-10s : %7s' % ("CORE ", core_result)+'\n')
-    add_qr_data('%-10s : %7s' % ("ART", art_result)+'\n')
+    add_qr_data('%-10s : %7s' % ("API ", api_result) + '\n')
+    add_qr_data('%-10s : %7s' % ("CORE ", core_result) + '\n')
+    add_qr_data('%-10s : %7s' % ("ART", art_result) + '\n')
 
-#Get EMC information
-def get_emc_versions(emc):
+# Get EMC information
+def get_emc_versions():
     qr.add_data("\n EMC CURRENT VERSION \n")
-    emc_result = run('cd ',_emc_dir | 'git describe --tags')
+    command = "git describe --tags"
+    emc_result = subprocess.Popen(command, shell=True, cwd='{}'.format(_emc_dir))
+    #print (emc_result)
     add_qr_data('%-10s : %7s' % ("EMC ", emc_result) + '\n')
 
 
-#Check for Installed Systems
-def check_systems ():
+# Check for Installed Systems
+def check_systems():
     if exists(_emc_dir):
-        #got to EMC unction
+        get_emc_versions()
     elif exists(_poc_api_dir, _poc_core_art_dir, _poc_core_dir):
-        get_poc_versions(_poc_core_dir, _poc_core_art_dir, _poc_api_dir)
-
-
-
-
-
+        get_poc_versions()
 
 
 def add_qr_data(input_data):
     qr.add_data(input_data)
     qr.make(fit=True)
     img = qr.make_image(fill='black', back_color='white')
-    img.save('qrcode001.png')
+    img.save('templates/static/images/toolbox.png')
 
+
+#@app.route('/')
+def index():
+    # qr.add_data("Hello, world!")
+    # qr.make(fit=True)
+    # img = qr.make_image(fill='black', back_color='red')
+    return render_template('index.html')
+
+
+#def get_image():
+    #image_name = 'templates/static/images/toolbox.png'
+    #Simage_name.Seek(0)
 
 
 @app.route('/')
-def index():
-    qr.add_data("Hello, world!")
-    qr.make(fit=True)
-    img = qr.make_image(fill='black', back_color='white')
-    return render_template('index.html')
-
-@app.route('/get-qr-Image')
-def get_image():
-    image_name = 'qrcode001.png'
-    #Simage_name.Seek(0)
-    return flask.send_file(image_name, mimetype='png')
-
-
 def main():
-    get_ram_details(psutil.virtual_memory().available)
+    get_ram_details(psutil.virtual_memory())
     get_disk_usage(psutil.disk_usage('/'))
     check_systems()
-    get_image()
 
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-    #main()
-
+    # main()
