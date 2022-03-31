e0b8e9c9e1de90329a8d560f75c72b8168efe41a
YOUR NAME <YOUR@EMAIL.com>
2022-03-31 10:42:17 +0200

    added setup.py

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,10 @@ FROM python:2.7-alpine
 ADD . /toolbox-host
 WORKDIR /toolbox-host
 COPY requirements.txt requirements.txt
-RUN python -m venv /env \
-    && /env/bin/pip install --upgrade pip \
-    && pip install -r requirements.txt
+RUN  sudo apt install virtualenv \
+     && virtualenv flask \
+     && source flask/bi/activate \
+     && pip install -r requirements.txt
 
 COPY . .
 
---
+++ setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+import os
+
+
+def configure_site():
+    """COnfigures the site"""
+    # install pip
+    print(" Step 1 : Update laptop")
+    answer = os.system("sudo apt-get update")
+    print("Step 2: Install Pip.")
+    answer1 = os.system("sudo apt install python-pip")
+    print(answer1)
+    print("Step3 : install pyhton environment")
+    os.system("sudo apt install virtualenv")
+    os.system("virtualenv flask")
+    os.system(". flask/bin/activate && pip install -r requirements.txt && python app.py")
+
+    return True
+
+
+def main():
+    configure_site()
+    return True
+
+
+if __name__ == '__main__':
+    main()
