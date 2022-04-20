5e2f943a906cddd4a8c245083a90dde7afdf643a
YOUR NAME <YOUR@EMAIL.com>
2022-04-20 09:46:56 +0200

    created Auto start functionality

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 
 def configure_site():
-    """COnfigures the site"""
+    #Configures the site
     # install pip
     print(" Step 1 : Update laptop")
     answer = os.system("sudo apt-get update")
@@ -12,30 +12,11 @@ def configure_site():
     print(answer1)
     print("Step3 : install python environment")
     os.system("sudo apt install virtualenv")
-    os.system("virtualenv flask")
-    os.system(
-        ". flask/bin/activate && pip install -r requirements.txt && FLASK_APP=app.py FLASK_ENV=development flask run --port 6070")
+    os.system("virtualenv flask")      
     print("creating Toolbox Service")
     # here is the code for creating the site.
-    os.system("cat > /etc/systemd/system/autotransfer.service <<EOF \
-            [Unit]"
-              "Description=EGPAF Auto Data Transfer Service"
-              "After=network.target"
-              "[Service]"
-              "WorkingDirectory=$INSTALL_DIR"
-              "ExecStart=$VPYTHONB ${INSTALL_DIR}/sms_send.py &> ${INSTALL_DIR}/send_sms.log"
-              "ExecStop=/bin/kill -INT \$MAINPID"
-              "ExecReload=/bin/kill -TERM \$MAINPID"
-              "# In case if it gets stopped, restart it immediately"
-              "Restart=always"
-              "Type=simple"
-
-              "[Install]"
-              "# multi-user.target corresponds to run level 3"
-              "# roughtly meaning wanted by system start"
-              "WantedBy=multi-user.target"
-              "EOF ")
-    os.system("sudo systemctl daemon-reload && sudo systemctl start autotransfer && sudo systemctl enable autotransfer")
+    os.system("sudo cp toolbox.service /etc/systemd/system/")
+    os.system("sudo systemctl daemon-reload && sudo systemctl start toolbox && sudo systemctl enable toolbox")
     return True
 
 
