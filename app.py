c3ca0df9908a2981bc43e923e303efe624271ba9
YOUR NAME <YOUR@EMAIL.com>
2022-05-24 11:19:40 +0200

    replaced / updated files

--- app.py
+++ app.py
@@ -1,113 +1,52 @@
 import json
-import os
+from flask import Flask, render_template
+from config.config import data
+from utils.export_emr_data import check_installation_folders
+from utils.generate_qr_image import add_qr_data
+from utils.setup_toolbox import mac_address
+from utils.utilities import load_file
+from utils.validate_emr_data import validate_config_file
 
-from flask import Flask, render_template, request
-
-from setup_facility_details import get_facility_name
-from utils.check_config import facility_details
-from utils.encrypt_make_qr_image import encrypt_make_qr_image
-from utils.emr_systems import emr_systems
-from utils.utils import load_sites
 
 app = Flask(__name__, static_folder="templates/static")
 
 
-@app.route('/home')
-def test():
-    """
-    Just Testing if the app is Alright and Reachable
-    :return: Json String.
-    """
-    return json.dumps({"status": "success", "message": "Welcome to Toolbox-host QR Service !!"}), 200
-
-
 @app.route('/')
-def get_qr_image():
-    """This is the entry point."""
-    result = make_qr_image()  # This calls a function that creates a QR image
-    if not result:
-        all_sites = load_sites("config/.sites.json")
-        return render_template('configure.html', sites=all_sites)
+def extract_data():
+    """gets EMR data and creates a QR Image
+    Args: None
+    Returns:
+        dict: hosts from api
+    """
+    # first verify if the data is correct in the config file
 
-    # get site name. So that it is displayed on the home page as well.
-    with open('config/config.json') as f:
-        site_name = json.load(f)
-        print(site_name["site_name"])
+    config_file_data = validate_config_file(data["config"])
+    if not config_file_data:
+        return render_template('error.html')  # this will load a page that informs the user to reconfigure toolbox
+    # if all is alright, then do the following
+    # 1. get EMR version and mac address
+    emr_data = check_installation_folders(data["apps_loc"])
+    mac = mac_address()
+    if not emr_data:
+        return render_template('error.html')
+    # 2. This is a final Dictionary to be sent for QR Image generation
+    final_emr_data = \
+        {
+            "1": "Toolbox",
+            "uuid": config_file_data["uuid"],
+            "app_id": config_file_data["app_id"],
+            "module": emr_data,
+            "mac": mac
+
+        }
+
+    final_string_to_decrypt = json.dumps(final_emr_data)
+    # 3. Add the data to QR Image
+    add_qr_data(final_string_to_decrypt, data["toolbox_image"])
+
+    # 4 Get facility name . This name will be displayed on UI
+    site_name = load_file(data["config"])
     return render_template('index.html', site_name=site_name["site_name"])
 
-
-@app.route('/config', methods=['GET', 'POST'])
-def configure():
-    if "POST" == request.method:
-        select = request.form.get('site')
-        select = json.dumps(select)
-        facility_details().save_facility_details(select)
-        return get_qr_image()
-
-    if "GET" == request.method:
-        all_sites = load_sites("config/.sites.json")
-        return render_template('configure.html', sites=all_sites)
-
-    # return render_template('index.html')
-
-
-@app.route('/reconfig')
-def re_configure():
-    os.system("source venv/bin/activate && python -c 'import setup_facility_details; "
-              "setup_facility_details.get_facility_name()'")
-    return os.system("source venv/bin/activate && python -c 'import setup_facility_details; "
-              "setup_facility_details.get_facility_name()'")
-
-@app.route('/getEmrData')
-def get_emr_data():
-    """
-    This function get all Information required on a sites(emr, memory utilization,
-    :return: a json file with all details
-    """
-    # first check if the config file is configured.
-    site_result = facility_details().get_facility_details()  # If all Site Information is correct.
-    if not site_result:  # If the file is  not complete. then the application will exit.
-
-        return False  # Render the Error page
-    else:  # if the config file is set correctly, continue checking the services
-        emr_result = emr_systems().check_systems()  # checks if site is running a POC or EMC system
-        # This is a final Dictionary to be sent for encryption
-        final_string_to_decrypt = \
-            {
-                "1": "Toolbox",
-                "uuid": site_result["uuid"],
-                "app_id": site_result["app_id"],
-                "module": emr_result
-
-            }
-
-        final_string_to_decrypt = json.dumps(final_string_to_decrypt)
-        return final_string_to_decrypt
-
-
-@app.route('/encrypt')
-def encrypt():
-    """
-    This method is encrypting the data and then forming a QR image
-    :return:
-    """
-    final_string_to_decrypt = get_emr_data()
-    # now send to QR IMAGE after Encrypting the final string
-    encrypted_data = encrypt_make_qr_image().encrypt_data("data", final_string_to_decrypt)
-
-    return str(encrypted_data)
-
-
-@app.route('/createImage')
-def make_qr_image():
-    final_string_to_decrypt = get_emr_data()
-    if final_string_to_decrypt:
-        result = encrypt_make_qr_image().add_qr_data(final_string_to_decrypt)  # NOTE : If there is
-        # another QR Image, it will be replaced when the code runs
-        return str(result)
-    return False
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=3030)
-
+    app.run(host='127.0.0.1', debug=True, port=6070)
--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #! /usr/bin/python
 import os
 
-
-from setup_facility_details import get_facility_name, mac_address
+from utils.setup_toolbox import mac_address, get_facility_name
 
 
 def configure_site():
+    """
+       configures toolbox
+       :return:
+       """
     # Configures the site
     # install pip
-    print(" Step 1 : Update laptop")
+    print("Step 1 : Update laptop")
     answer = os.system("sudo apt-get update")
     print("Step 2: Install Pip.")
     answer1 = os.system("sudo apt install python-pip")
@@ -16,31 +19,30 @@ def configure_site():
     print("Step3 : install python environment")
     os.system("sudo apt install virtualenv")
     os.system("virtualenv flask")
-    print("creating Toolbox Service")
-    # here is the code for creating the site.
-    print("********************")
-    print("SET UP FACILITY DETAILS")
-    get_facility_name()
-    print("SITE IS NOW CONFIGURED !!!")
-    print("********************")
-    os.system("sudo cp toolbox.service /etc/systemd/system/")
-    os.system("sudo systemctl daemon-reload && sudo systemctl start toolbox && sudo systemctl enable toolbox")
     print("*********** SETTING FACILITY DETAILS *****************")
-    os.system(". flask/bin/activate && python -c 'import setup_facility_details;  "
-              "setup_facility_details.get_facility_name()'")
+    os.system(". flask/bin/activate && pip install -r requirements.txt && sudo apt-get install git")
+
+    print("******************************************************")
+    os.system(". flask/bin/activate && python -c 'from utils.setup_toolbox import "
+              "get_facility_name; "
+              "get_facility_name()'")
     mac = mac_address()
     print(mac)
     print("*********** END - Facility Configured Successfully *****************")
-    return True
-
-
-def create_service():
-    """ Create Service """
-    os.system("")
+    print("creating Toolbox Service")
+    os.system("sudo cp toolbox.desktop ~/Desktop/")
+    # here is the code for creating the site.
+    os.system("sudo cp toolbox.service /etc/systemd/system/")
+    os.system("sudo systemctl daemon-reload && sudo systemctl start toolbox && sudo systemctl enable toolbox")
+    print("FINISHED :creating Toolbox Service")
     return True
 
 
 def main():
+    """
+    startup function
+    :return: boolean
+    """
     configure_site()
     return True
 
